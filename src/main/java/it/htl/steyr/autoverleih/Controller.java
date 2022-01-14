ad70964289e0b03cf148a5263510aea6ddf509b6
mjaeger293 <mjaeger@htl-steyr.ac.at>
2022-01-14 15:37:05 +0100

    Startseite
    Autos filtern
    
    Verleihzeitraum festlegen

--- src/main/java/it/htl/steyr/autoverleih/Controller.java
+++ src/main/java/it/htl/steyr/autoverleih/Controller.java
@@ -1,19 +1,88 @@
 package it.htl.steyr.autoverleih;
 
+import it.htl.steyr.autoverleih.model.*;
+import it.htl.steyr.autoverleih.model.repositories.CarRepository;
+import it.htl.steyr.autoverleih.model.repositories.ManufacturerRepository;
+import it.htl.steyr.autoverleih.model.repositories.ModelRepository;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
 
 @Component
-public class Controller {
+public class Controller extends Administration {
+
+    public DatePicker rentalDatePicker;
+    public DatePicker returnDatePicker;
+    public ComboBox<Manufacturer> manufacturerFilterComboBox;
+    public ComboBox<Model> modelFilterComboBox;
+    public TableView<Car> availableCarsTableView;
+
+    boolean manufacturerFilterVisible = false;
+    boolean modelFilterVisible = false;
+    List<Car> availableCars;
+
+    Manufacturer selectedManufacturer;
+    Model selectedModel;
+
+    @Autowired
+    CarRepository carRepository;
+
+    @Autowired
+    ManufacturerRepository manufacturerRepository;
+
+    @Autowired
+    ModelRepository modelRepository;
+
+    public void initialize() {
+        manufacturerFilterComboBox.getItems().setAll(manufacturerRepository.findAll());
+
+        availableCarsTableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        TableColumn<Car, Integer> idColumn =
+                createTableColumn("ID", "id");
+
+        TableColumn<Car, Model> carModelTableColumn =
+                createTableColumn("Modell", "model");
+
+        TableColumn<Car, String> carColorTableColumn =
+                createTableColumn("Farbe", "color");
+
+        TableColumn<Car, Integer> carHorsePowerTableColumn =
+                createTableColumn("PS", "horsePower");
+
+        TableColumn<Car, String> carLicensePlateColumn =
+                createTableColumn("Kennzeichen", "licensePlate");
+
+        TableColumn<Car, Transmission> carTransmissionTableColumn =
+                createTableColumn("Getriebe", "transmission");
+
+        TableColumn<Car, Fuel> carFuelTableColumn =
+                createTableColumn("Treibstoff", "fuel");
+
+        // Add all columns
+        availableCarsTableView.getColumns().add(idColumn);
+        availableCarsTableView.getColumns().add(carModelTableColumn);
+        availableCarsTableView.getColumns().add(carColorTableColumn);
+        availableCarsTableView.getColumns().add(carHorsePowerTableColumn);
+        availableCarsTableView.getColumns().add(carLicensePlateColumn);
+        availableCarsTableView.getColumns().add(carTransmissionTableColumn);
+        availableCarsTableView.getColumns().add(carFuelTableColumn);
+    }
 
     public void CloseItemClicked(ActionEvent actionEvent) {
         Platform.exit();
@@ -54,6 +123,70 @@ public class Controller {
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void rentalTimeChanged(ActionEvent actionEvent) {
+        LocalDate localRentalDate = rentalDatePicker.getValue();
+        LocalDate localReturnDate = returnDatePicker.getValue();
+
+        if (rentalDatePicker.getValue() != null && returnDatePicker.getValue() != null) {
+            Date rentalDate = Date.from(Instant.from(localRentalDate.atStartOfDay(ZoneId.systemDefault())));
+            Date returnDate = Date.from(Instant.from(localReturnDate.atStartOfDay(ZoneId.systemDefault())));
+
+            availableCars = carRepository.getCarsNotRented(rentalDate, returnDate);
+
+            availableCarsTableView.getItems().clear();
+
+            if (availableCars.size() > 0) {
+                availableCarsTableView.getItems().addAll(availableCars);
+            }
+        }
+    }
+
+    public void manufacturerFilterChanged(ActionEvent actionEvent) {
+        selectedManufacturer = manufacturerFilterComboBox.getSelectionModel().getSelectedItem();
+
+        if (selectedManufacturer != null) {
+            modelFilterComboBox.getItems().setAll(modelRepository.findByManufacturer(selectedManufacturer));
+            modelFilterComboBox.setDisable(false);
+
+            availableCarsTableView.getItems().clear();
+
+            for (Car c : availableCars) {
+                if (c.getModel().getManufacturer().getId() == selectedManufacturer.getId()) {
+                    availableCarsTableView.getItems().add(c);
+                }
+            }
+        }
+    }
+
+    public void modelFilterChanged(ActionEvent actionEvent) {
+        selectedModel = modelFilterComboBox.getSelectionModel().getSelectedItem();
+
+        if (selectedModel != null) {
+            availableCarsTableView.getItems().clear();
+
+            for (Car c : availableCars) {
+                if (c.getModel().getId() == selectedModel.getId()) {
+                    availableCarsTableView.getItems().add(c);
+                }
+            }
+        }
+    }
+
+    public void clearFiltersClicked(ActionEvent actionEvent) {
+        modelFilterComboBox.setDisable(true);
+        modelFilterComboBox.getItems().clear();
+
+        availableCarsTableView.getItems().setAll(availableCars);
+    }
+
+    public void availableCarsTableViewClicked(MouseEvent mouseEvent) {
+        Car selectedCar = availableCarsTableView.getSelectionModel().getSelectedItem();
+
+        if (mouseEvent.getClickCount() == 2 && selectedCar != null) {
+            //openEditWindow(mouseEvent, selectedCar);
         }
     }
 }
--- src/main/java/it/htl/steyr/autoverleih/model/repositories/CarRepository.java
+++ src/main/java/it/htl/steyr/autoverleih/model/repositories/CarRepository.java
@@ -5,7 +5,7 @@ import it.htl.steyr.autoverleih.model.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 
 public interface CarRepository extends JpaRepository<Car, Integer> {
@@ -14,6 +14,6 @@ public interface CarRepository extends JpaRepository<Car, Integer> {
 
     List<Car> findByModel(Model model);
 
-    //@Query(value = "SELECT * FROM mjaeger_car WHERE id NOT in (SELECT car FROM mjaeger_transactions WHERE rental_date < ?1 AND return_date > ?2 GROUP BY owner)", nativeQuery = true)
-    //List<Car> getCarsNotRented(Date rental_date, Date return_date);
+    @Query(value = "SELECT * FROM mjaeger_car WHERE id NOT in (SELECT car FROM mjaeger_rental WHERE (rental_date > ?1 AND return_date < ?2) OR (return_date > ?1 AND return_date < ?2) OR (rental_date < ?1 AND return_date > ?2))", nativeQuery = true)
+    List<Car> getCarsNotRented(Date rental_date, Date return_date);
 }
